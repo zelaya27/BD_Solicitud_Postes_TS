1bccd330dcacf3d937524d3e33d7d23b4fba3849
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-21 19:23:53 -0600

    Update API endpoint in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: "SOLICITUD DE MATERIALES",
   ENDPOINTS: {
-    API: "https://script.google.com/macros/s/AKfycbzCTj8NhQmwztqCY7Xn8I25x7ibLq6MZ5f0gO8PPJCkAujOIvogPl8tsubCs_yLgPbyfg/exec"
+    API: "https://script.google.com/macros/s/AKfycbwElxYJm_1ziKnubppR7SROS6-gDZlJ4mLoggCInLPy0k85IhOkyjkwEHwaaO5ulESYMw/exec"
   }
 };
