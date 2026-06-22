778e0bb80b7f2075ce6d11fcee49fc0dc2fd9b3d
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-21 18:40:17 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: "SOLICITUD DE MATERIALES",
   ENDPOINTS: {
-    API: "https://script.google.com/macros/s/AKfycbx-MxC9uOty5Jt0eBT8jaGFz6GfkM-cIPg6z5A1-gfDuu_bCK_VsrPDLIz0lHn_TNtH-w/exec"
+    API: "https://script.google.com/macros/s/AKfycbzCTj8NhQmwztqCY7Xn8I25x7ibLq6MZ5f0gO8PPJCkAujOIvogPl8tsubCs_yLgPbyfg/exec"
   }
 };
