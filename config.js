b8c7691fcf12203af43e39d1a0f314b6b332ed01
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-21 19:07:11 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: "SOLICITUD DE MATERIALES",
   ENDPOINTS: {
-    API: "https://script.google.com/macros/s/AKfycbzKP1Yfqj7C47DiCG0V3fons0Lq-hh4uJm_fqXH3TV33v6qZb9XGfpj8fZJepRZ6MOI/exec"
+    API: "https://script.google.com/macros/s/AKfycbzcylkviHcJ1cUF95pjcOHnOPqBAdOXsGbFLccCe9dNeXvUrtCzIVDkkwMk1xiqTYCr0g/exec"
   }
 };
